b63e5b61b0c1b5fd0a0a2997f6116ea789c7bd14
tradplus <58809719+tradplus@users.noreply.github.com>
2023-08-21 16:03:48 +0800

    隐私相关

--- TradPlusAndroidDemo/app/src/main/java/com/tradplus/privacy/CSJCustomUserData.java
+++ TradPlusAndroidDemo/app/src/main/java/com/tradplus/privacy/CSJCustomUserData.java
@@ -1,5 +1,6 @@
 package com.tradplus.privacy;
 
+import com.bytedance.sdk.openadsdk.LocationProvider;
 import com.bytedance.sdk.openadsdk.TTCustomController;
 
 // 国内隐私信息控制开关
@@ -8,32 +9,56 @@ public class CSJCustomUserData extends TTCustomController {
 
     // 是否允许SDK主动使用地理位置信息
     public boolean isCanUseLocation() {
-        return false;
+        return true;
     }
 
-    // 是否允许SDK主动使用手机硬件参数，如：imei
-    public boolean isCanUsePhoneState() {
-        return false;
-    }
-
-    // 是否允许SDK主动获取ANDROID_ID
-    public boolean isCanUseAndroidId() {
-        return false;
-    }
-
-    //是否允许SDK主动使用ACCESS_WIFI_STATE权限
-    public boolean isCanUseWifiState() {
-        return false;
-    }
-
-    //是否允许SDK主动使用WRITE_EXTERNAL_STORAGE权限
-    public boolean isCanUseWriteExternal() {
-        return false;
+    public LocationProvider getTTLocation() {
+        return null;
     }
 
     // 是否允许SDK主动获取设备上应用安装列表的采集权限
     public boolean alist() {
-        return false;
+        return true;
+    }
+
+    // 是否允许SDK主动使用手机硬件参数，如：imei
+    public boolean isCanUsePhoneState() {
+        return true;
+    }
+
+    public String getDevImei() {
+        return null;
+    }
+
+    //是否允许SDK主动使用ACCESS_WIFI_STATE权限
+    public boolean isCanUseWifiState() {
+        return true;
+    }
+
+    public String getMacAddress() {
+        return null;
+    }
+
+    //是否允许SDK主动使用WRITE_EXTERNAL_STORAGE权限
+    public boolean isCanUseWriteExternal() {
+        return true;
+    }
+
+    public String getDevOaid() {
+        return null;
+    }
+
+    // 是否允许SDK主动获取ANDROID_ID
+    public boolean isCanUseAndroidId() {
+        return true;
+    }
+
+    public String getAndroidId() {
+        return null;
+    }
+
+    public boolean isCanUsePermissionRecordAudio() {
+        return true;
     }
 
 }
