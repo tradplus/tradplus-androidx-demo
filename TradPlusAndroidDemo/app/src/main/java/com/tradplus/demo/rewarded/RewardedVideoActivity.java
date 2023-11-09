ab9875dd9e7c02cc1b0e3a665b338281f5a61c16
tradplus <58809719+tradplus@users.noreply.github.com>
2023-11-09 14:39:07 +0800

    10.5.0.1

--- TradPlusAndroidDemo/app/src/main/java/com/tradplus/demo/rewarded/RewardedVideoActivity.java
+++ TradPlusAndroidDemo/app/src/main/java/com/tradplus/demo/rewarded/RewardedVideoActivity.java
@@ -49,126 +49,129 @@ public class RewardedVideoActivity extends AppCompatActivity implements View.OnC
         findViewById(R.id.btn_show).setOnClickListener(this);
         findViewById(R.id.second_page).setOnClickListener(this);
 
-        initRewardAd();
     }
 
     private void initRewardAd() {
-        mTpReward = new TPReward(this,TestAdUnitId.REWRDVIDEO_ADUNITID);
+        if (mTpReward == null) {
+            mTpReward = new TPReward(this, TestAdUnitId.REWRDVIDEO_ADUNITID);
 
-        //进入广告场景，广告场景ID后台创建
-        // 广告场景是用来统计进入广告场景的次数和进入场景后展示广告的次数，所以请在准确的位置调用
-        mTpReward.entryAdScenario(TestAdUnitId.ENTRY_AD_REWARD);
+            //进入广告场景，广告场景ID后台创建
+            // 广告场景是用来统计进入广告场景的次数和进入场景后展示广告的次数，所以请在准确的位置调用
+            mTpReward.entryAdScenario(TestAdUnitId.ENTRY_AD_REWARD);
 
-        // 流量分组的时候用到，可以自定义一些app相关的属性，在TradPlus后台根据这些属性来对用户做分组
-        // 设置流量分组有两个维度，一个是全局的，一个是单个广告位的，单个广告位的相同属性会覆盖全局的
-        HashMap<String, String> customMap = new HashMap<>();
-        customMap.put("user_gender",  "male");//男性
-        customMap.put("user_level", "10");//游戏等级10
+            // 流量分组的时候用到，可以自定义一些app相关的属性，在TradPlus后台根据这些属性来对用户做分组
+            // 设置流量分组有两个维度，一个是全局的，一个是单个广告位的，单个广告位的相同属性会覆盖全局的
+            HashMap<String, String> customMap = new HashMap<>();
+            customMap.put("user_gender", "male");//男性
+            customMap.put("user_level", "10");//游戏等级10
 //        SegmentUtils.initCustomMap(customMap);//设置APP维度的规则，对全部placement有效
-        SegmentUtils.initPlacementCustomMap(TestAdUnitId.REWRDVIDEO_ADUNITID, customMap);//仅对该广告位有效，会覆盖APP维度设置的规则
+            SegmentUtils.initPlacementCustomMap(TestAdUnitId.REWRDVIDEO_ADUNITID, customMap);//仅对该广告位有效，会覆盖APP维度设置的规则
 
-        // 监听广告的不同状态
-        mTpReward.setAdListener(new RewardAdListener() {
-            @Override
-            public void onAdLoaded(TPAdInfo tpAdInfo) {
-                Log.i(TAG, "onAdLoaded: ");
-                Toast.makeText(RewardedVideoActivity.this, "广告加载成功", Toast.LENGTH_SHORT).show();
-            }
+            // 监听广告的不同状态
+            mTpReward.setAdListener(new RewardAdListener() {
+                @Override
+                public void onAdLoaded(TPAdInfo tpAdInfo) {
+                    Log.i(TAG, "onAdLoaded: ");
+                    Toast.makeText(RewardedVideoActivity.this, "广告加载成功", Toast.LENGTH_SHORT).show();
+                }
 
-            @Override
-            public void onAdClicked(TPAdInfo tpAdInfo) {
-                Log.i(TAG, "onAdClicked: 广告"+ tpAdInfo.adSourceName +"被点击");
-                Toast.makeText(RewardedVideoActivity.this, "广告被点击", Toast.LENGTH_SHORT).show();
-            }
+                @Override
+                public void onAdClicked(TPAdInfo tpAdInfo) {
+                    Log.i(TAG, "onAdClicked: 广告" + tpAdInfo.adSourceName + "被点击");
+                    Toast.makeText(RewardedVideoActivity.this, "广告被点击", Toast.LENGTH_SHORT).show();
+                }
 
-            @Override
-            public void onAdImpression(TPAdInfo tpAdInfo) {
-                Log.i(TAG, "onAdImpression: 广告"+ tpAdInfo.adSourceName +"展示");
-                Toast.makeText(RewardedVideoActivity.this, "广告展示", Toast.LENGTH_SHORT).show();
+                @Override
+                public void onAdImpression(TPAdInfo tpAdInfo) {
+                    Log.i(TAG, "onAdImpression: 广告" + tpAdInfo.adSourceName + "展示");
+                    Toast.makeText(RewardedVideoActivity.this, "广告展示", Toast.LENGTH_SHORT).show();
 
-            }
+                }
 
-            @Override
-            public void onAdFailed(TPAdError tpAdError) {
-                Log.i(TAG, "onAdFailed: ");
-                Toast.makeText(RewardedVideoActivity.this, "广告加载失败", Toast.LENGTH_SHORT).show();
-            }
+                @Override
+                public void onAdFailed(TPAdError tpAdError) {
+                    Log.i(TAG, "onAdFailed: " + tpAdError.getErrorCode() + ",MSG :" + tpAdError.getErrorMsg());
+                    Toast.makeText(RewardedVideoActivity.this, "广告加载失败", Toast.LENGTH_SHORT).show();
+                }
 
-            @Override
-            public void onAdClosed(TPAdInfo tpAdInfo) {
-                Log.i(TAG, "onAdClosed: 广告"+ tpAdInfo.adSourceName +"被关闭");
-                Toast.makeText(RewardedVideoActivity.this, "广告关闭", Toast.LENGTH_SHORT).show();
+                @Override
+                public void onAdClosed(TPAdInfo tpAdInfo) {
+                    Log.i(TAG, "onAdClosed: 广告" + tpAdInfo.adSourceName + "被关闭");
+                    Toast.makeText(RewardedVideoActivity.this, "广告关闭", Toast.LENGTH_SHORT).show();
 
-            }
+                }
 
-            @Override
-            public void onAdReward(TPAdInfo tpAdInfo) {
-                Log.i(TAG, "onAdReward: 奖励项目：" + tpAdInfo.currencyName +" ， 奖励数量："+ tpAdInfo.amount);
-                Toast.makeText(RewardedVideoActivity.this, "广告奖励", Toast.LENGTH_SHORT).show();
-                // 给用户发放奖励
-                // 在V6.4.5以后的版本，可以根据三方广告平台的文档来增加奖励验证功能（奖励验证需要开发者后台配合，TradPlus不提供后台服务奖励验证的功能）
-            }
+                @Override
+                public void onAdReward(TPAdInfo tpAdInfo) {
+                    Log.i(TAG, "onAdReward: 奖励项目：" + tpAdInfo.currencyName + " ， 奖励数量：" + tpAdInfo.amount);
+                    Toast.makeText(RewardedVideoActivity.this, "广告奖励", Toast.LENGTH_SHORT).show();
+                    // 给用户发放奖励
+                    // 在V6.4.5以后的版本，可以根据三方广告平台的文档来增加奖励验证功能（奖励验证需要开发者后台配合，TradPlus不提供后台服务奖励验证的功能）
+                }
 
-            @Override
-            public void onAdVideoStart(TPAdInfo tpAdInfo) {
-                // V8.1.0.1 播放开始
-            }
+                @Override
+                public void onAdVideoStart(TPAdInfo tpAdInfo) {
+                    // V8.1.0.1 播放开始
+                }
 
-            @Override
-            public void onAdVideoEnd(TPAdInfo tpAdInfo) {
-                // V8.1.0.1 播放结束
-            }
+                @Override
+                public void onAdVideoEnd(TPAdInfo tpAdInfo) {
+                    // V8.1.0.1 播放结束
+                }
 
-            @Override
-            public void onAdVideoError(TPAdInfo tpAdInfo, TPAdError tpAdError) {
+                @Override
+                public void onAdVideoError(TPAdInfo tpAdInfo, TPAdError tpAdError) {
 
-            }
+                }
 
-        });
+            });
 
-        // 监听每一层广告的加载情况，非特殊需求可以不实现
-        mTpReward.setAllAdLoadListener(new LoadAdEveryLayerListener() {
-            @Override
-            public void onAdAllLoaded(boolean b) {
-                Log.i(TAG, "onAdAllLoaded: 该广告位下所有广告加载结束，是否有广告加载成功 ：" + b);
-            }
+            // 监听每一层广告的加载情况，非特殊需求可以不实现
+            mTpReward.setAllAdLoadListener(new LoadAdEveryLayerListener() {
+                @Override
+                public void onAdAllLoaded(boolean b) {
+                    Log.i(TAG, "onAdAllLoaded: 该广告位下所有广告加载结束，是否有广告加载成功 ：" + b);
+                }
 
-            @Override
-            public void oneLayerLoadFailed(TPAdError tpAdError, TPAdInfo tpAdInfo) {
-                Log.i(TAG, "oneLayerLoadFailed:  广告"+ tpAdInfo.adSourceName +" 加载失败，code :: "+
-                        tpAdError.getErrorCode() +" , Msg :: "+ tpAdError.getErrorMsg());
-            }
+                @Override
+                public void oneLayerLoadFailed(TPAdError tpAdError, TPAdInfo tpAdInfo) {
+                    Log.i(TAG, "oneLayerLoadFailed:  广告" + tpAdInfo.adSourceName + " 加载失败，code :: " +
+                            tpAdError.getErrorCode() + " , Msg :: " + tpAdError.getErrorMsg());
+                }
 
-            @Override
-            public void oneLayerLoaded(TPAdInfo tpAdInfo) {
-                Log.i(TAG, "oneLayerLoaded:  广告"+ tpAdInfo.adSourceName +" 加载成功");
-            }
+                @Override
+                public void oneLayerLoaded(TPAdInfo tpAdInfo) {
+                    Log.i(TAG, "oneLayerLoaded:  广告" + tpAdInfo.adSourceName + " 加载成功");
+                }
 
-            @Override
-            public void onAdStartLoad(String s) {
-                // 每次调用load方法时返回的回调，包含自动加载等触发时机。V7.9.0 新增。
-            }
+                @Override
+                public void onAdStartLoad(String s) {
+                    // 每次调用load方法时返回的回调，包含自动加载等触发时机。V7.9.0 新增。
+                }
 
-            @Override
-            public void oneLayerLoadStart(TPAdInfo tpAdInfo) {
-                // 每层waterfall 向三方广告源发起请求前，触发的回调。V7.9.0 新增。
-            }
+                @Override
+                public void oneLayerLoadStart(TPAdInfo tpAdInfo) {
+                    // 每层waterfall 向三方广告源发起请求前，触发的回调。V7.9.0 新增。
+                }
 
-            @Override
-            public void onBiddingStart(TPAdInfo tpAdInfo) {
+                @Override
+                public void onBiddingStart(TPAdInfo tpAdInfo) {
 
-            }
+                }
 
-            @Override
-            public void onBiddingEnd(TPAdInfo tpAdInfo, TPAdError tpAdError) {
+                @Override
+                public void onBiddingEnd(TPAdInfo tpAdInfo, TPAdError tpAdError) {
 
-            }
+                }
 
-            @Override
-            public void onAdIsLoading(String s) {
-               // 调用load之后如果收到此回调，说明广告位仍处于加载状态，无法触发新的一轮广告加载。V 9.0.0.1新增
-            }
-        });
+                @Override
+                public void onAdIsLoading(String s) {
+                    // 调用load之后如果收到此回调，说明广告位仍处于加载状态，无法触发新的一轮广告加载。V 9.0.0.1新增
+                }
+            });
+        }
+        videoUtils.loadReward(mTpReward);
+
     }
 
     @Override
@@ -184,9 +187,7 @@ public class RewardedVideoActivity extends AppCompatActivity implements View.OnC
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_load:
-                if (mTpReward != null) {
-                    videoUtils.loadReward(mTpReward);
-                }
+                initRewardAd();
                 break;
             case R.id.btn_show:
                 if (videoUtils.isReady()) {
